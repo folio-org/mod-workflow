2ced50b3a727feaf479a9a56b3256fc84e2fc193
Kevin Day <kday@tamu.edu>
2024-11-07 17:00:25 -0600

    WIP: 2024/11/07 - 2.

--- components/src/main/java/org/folio/rest/workflow/model/converter/AbstractConverter.java
+++ components/src/main/java/org/folio/rest/workflow/model/converter/AbstractConverter.java
@@ -3,6 +3,8 @@ package org.folio.rest.workflow.model.converter;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.StreamReadFeature;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import jakarta.persistence.AttributeConverter;
@@ -14,6 +16,8 @@ public abstract class AbstractConverter<T> implements AttributeConverter<T, Stri
 
   private ObjectMapper objectMapper = JsonMapper.builder()
     .enable(StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION)
+    .enable(MapperFeature.REQUIRE_TYPE_ID_FOR_SUBTYPES)
+    .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
     .build();
 
   @Override
