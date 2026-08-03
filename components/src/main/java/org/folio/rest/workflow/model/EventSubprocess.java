c0c493d11ceaf8df751ea8447243d3524cea3195
Kevin Day <kday@tamu.edu>
2026-08-03 11:13:47 -0500

    MODWRKFLOW-73: Delete all sub-nodes when deleting a workflow.
    Add a new DTO with associated unit test for providing the ID and nodes.
    
    Get the nodes from this workflow and delete those first.
    
    The database structure is generated and there is no repo or anything like that to directly call for deletion.
    The nodes are not associated with foreign keys in the database across all fields.
    The `ON DELETE CASCADE` behavior is not useful, except for the embeddables.
    
    Add on delete actions defaults where possible.
    
    Avoid addng `@ManyToOne` or `@OneToMany` to avoid possible regressions, such as performance regressions.
    The goal here is not to redesign the database structure.
    
    Use the entity manager to perform custom **SQL** that shall perform the deletions.
    The list of nodes on the **Workflow** is not the complete list of nodes.
    Therefore, each entity that has a `_nodes` table must also be recursively parsed.
    
    Use existing code to help build and dynamically construct the appropriate **SQL**.
    The `DeserializeAsNodeJsonResolver.CLASSES` should probably be move to another class.
    Doing so would introduce more changes than desired for now so this is avoided.
    
    Add a debug logger to make the deletions easier to confirm/deny.
    
    Remove the invalid/outdated comment "To be removed. This is just an experiment.".

--- components/src/main/java/org/folio/rest/workflow/model/EventSubprocess.java
+++ components/src/main/java/org/folio/rest/workflow/model/EventSubprocess.java
@@ -1,6 +1,8 @@
 package org.folio.rest.workflow.model;
 
+import jakarta.persistence.AssociationOverride;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
 
 @Entity
 public class EventSubprocess extends AbstractProcess {
